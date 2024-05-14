57406bc1398df3764439b8a3db3f36a1e9a0278e
MdRaihan3 <1mdr91012@gmail.com>
2024-05-14 10:17:30 +0600

    updated

--- index.js
+++ index.js
@@ -132,7 +132,7 @@ async function run() {
             res.send(result)
         })
 
-        // update food
+        // update food safjsadl;fjasf
         app.patch('/update/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
@@ -144,7 +144,7 @@ async function run() {
             res.send(result)
         })
 
-        // delete food slfjasdlfsadkljfs
+        // delete food
         app.delete('/delete/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: new ObjectId(id) }
